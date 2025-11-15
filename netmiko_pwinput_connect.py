d3690200fad1f3f5fe1e8b2cd7f3c4bed446e62d
Ferds <ferds.formanes@gmail.com>
2025-11-15 12:25:01 +0800

    Initial commit

---
+++ netmiko_pwinput_connect.py
@@ -0,0 +1,23 @@
+from netmiko import ConnectHandler
+from pwinput import pwinput
+import pandas as pd
+
+
+username = input("Enter your username: ")
+password = pwinput("Enter your password: ", mask='*')
+
+device = {
+    "device_type": "cisco_xr",
+    "host": "sandbox-iosxr-1.cisco.com",
+    "username": username,
+    "password": password,
+}
+
+net_connect = ConnectHandler(**device)
+output = net_connect.send_command("show version", use_textfsm=True)
+
+df = pd.DataFrame(output)
+print(df)
+df.to_excel("show_version.xlsx", index=False)
+
+net_connect.disconnect()
